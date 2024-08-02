a1eb19bfe1bad680b7448c7efc9c122bb2801db1
Julia Eason <jmeason00@gmail.com>
2024-08-02 18:08:50 -0400

    adding contact

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,1 +1,25 @@
-// import 
+// import
+
+export default function Contact() {
+    return (
+        <div>
+            <div>
+
+        {/* //email */}
+    <a href="mailto:jmeason00@gmail.com">My Email</a>
+        {/* //github */}
+        <div>
+            <a href="https://github.com/jmeason"> GitHub </a>
+            </div>
+        
+
+        {/* //linkedin */}
+            <a href="www.linkedin.com/in/juliaeason220" > LinkedIn </a>
+        
+                
+
+            </div>
+        </div>
+
+    )
+}
