33fe32454240471760246137331cfafd4b538674
Julia Eason <jmeason00@gmail.com>
2024-08-02 17:27:49 -0400

    changing folders and files

--- src/components/PortfolioContainer.jsx
+++ src/components/PortfolioContainer.jsx
@@ -1,1 +1,35 @@
 //fjkd
+// This file is adapted from module 20 activity 22
+import { useState } from 'react';
+import Header from './Header';
+// import Nav from './Nav';
+import Portfolio from './pages/Portfolio';
+import About from './pages/About';
+import Resume from './pages/Resume';
+import Contact from './pages/Contact';
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState('About');
+
+  const renderPage = () => {
+    if (currentPage === 'About') {
+      return <About />;
+    }
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
+    }
+    if (currentPage === 'Contact') {
+      return <Contact />;
+    }
+    return <Resume />;
+  }
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      <Header currentPage={currentPage} handlePageChange={handlePageChange} />
+      <section className="mx-3">{renderPage()}</section>
+    </div>
+  )
+}
